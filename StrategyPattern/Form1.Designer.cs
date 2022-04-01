8c17b6f11525b14c4b74cd8b1bd55b72f41073b9
Tsviga Mariia <tsvigam@gmail.com>
2022-04-01 23:12:49 +1100

    Initial commit

---
+++ StrategyPattern/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace StrategyPattern
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.radioButton3);
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Location = new System.Drawing.Point(64, 30);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(289, 135);
+            this.panel1.TabIndex = 0;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(14, 83);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(107, 21);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Text on high";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(14, 56);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(113, 21);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Text on lower";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(14, 29);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(112, 21);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Input number";
+            this.radioButton1.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(64, 194);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(289, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(165, 243);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Check";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(411, 304);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ StrategyPattern/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StrategyPattern
+{
+    public partial class Form1 : Form
+    {
+        Context context;
+        public Form1()
+        {
+            InitializeComponent();
+            context = new Context(new NumberStrategy());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                context.SetStrategy(new NumberStrategy());
+            }
+            if (radioButton2.Checked)
+                context.SetStrategy(new LowerStrategy());
+            if (radioButton3.Checked)
+                context.SetStrategy(new HighStrategy());
+            context.ExecuteAlgorithm(textBox1.Text);
+        }
+    }
+
+    public class Context
+    {
+        IStrategy strategy;
+
+        public Context(IStrategy s)
+        {
+            strategy = s;
+        }
+
+        public void SetStrategy(IStrategy s)
+        {
+            strategy = s;
+        }
+
+        public void ExecuteAlgorithm(string s)
+        {
+            strategy.Algorithm(s);
+
+        }
+    }
+
+    public interface IStrategy
+    {
+        void Algorithm(string s);
+    }
+
+    public class NumberStrategy : IStrategy
+    {
+        public void Algorithm(string s)
+        {
+            if (s == null)
+                MessageBox.Show("Input somesing");
+            else
+            {
+                foreach (char c in s)
+                {
+                    if (!(c >= '0' && c <= '9'))
+                    {
+                        MessageBox.Show("Not number!");
+                        return;
+                    }
+                }
+                MessageBox.Show("True!");
+            }
+        }
+    }
+
+    public class LowerStrategy : IStrategy
+    {
+        public void Algorithm(string s)
+        {
+            if (s == null)
+                MessageBox.Show("Input somesing");
+            else
+            {
+                foreach (char c in s)
+                {
+                    if (!(c >= 'a' && c <= 'z'))
+                    {
+                        MessageBox.Show("Not lower string!");
+                        return;
+                    }
+                }
+                MessageBox.Show("True!");
+            }
+        }
+    }
+
+    public class HighStrategy : IStrategy
+    {
+        public void Algorithm(string s)
+        {
+            if (s == null)
+                MessageBox.Show("Input somesing");
+            else
+            {
+                foreach (char c in s)
+                {
+                    if (!(c >= 'A' && c <= 'Z'))
+                    {
+                        MessageBox.Show("Not high string!");
+                        return;
+                    }
+                }
+                MessageBox.Show("True!");
+            }
+        }
+    }
+}
